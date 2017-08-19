7f24566e7f4c7c89a78a2dc089bc2389c6b8f98f
Luca Stefani <luca020400@lineageos.org>
2017-08-19 13:17:46 +0200

    land: Inherit from Lineage vendor

--- lineage.mk
+++ lineage.mk
@@ -20,10 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from land device
-$(call inherit-product, device/xiaomi/land/device.mk)
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := land
