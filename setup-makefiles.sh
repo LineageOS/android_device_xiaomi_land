4cc73921d791c925665f742c71a9478cafdcecb6
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-07 15:34:00 -0400

    land: Generate treble-compatible makefiles
    Change-Id: I95fb9a2f30067c25a9d2ed3a235907959befcd90

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -43,7 +43,7 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 write_headers
 
 # Device blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt
+write_makefiles "$MY_DIR"/proprietary-files.txt true
 
 # Finish
 write_footers
