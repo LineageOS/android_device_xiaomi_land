af1d1489de19c8f63b1f46981437a07123c5cdee
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2018-06-13 19:50:20 +0530

    land: Move shim to vendor
    Change-Id: Ibbbba96be2758cc155f7596f1442d6d28ab7610b

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
+#               2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,10 +22,9 @@ LOCAL_SRC_FILES := \
     bionic/pthread_cond.cpp
 
 LOCAL_SHARED_LIBRARIES := libc
-
 LOCAL_MODULE := libshims_camera
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_32_BIT_ONLY := true
+LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
