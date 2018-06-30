171e4b65956e70b2eb72611994d62dbeeb4dbefd
Isaac Chen <isaacchen@isaacchen.cn>
2018-06-30 13:33:33 +0200

    land: Decommonize mixer path

--- device.mk
+++ device.mk
@@ -22,7 +22,8 @@ $(call inherit-product-if-exists, vendor/xiaomi/land/land-vendor.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
+    $(DEVICE_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(DEVICE_PATH)/audio/mixer_paths_qrd_sku1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_qrd_sku1.xml
 
 # Camera
 PRODUCT_PACKAGES += \
