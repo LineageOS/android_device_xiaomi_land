0899912776fc1a5ab30d6ffe07cc4274a0c29043
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2018-12-25 18:36:52 +0530

    land: Hex edit sensor_modules library for O
    Change-Id: Ic1468ded7b76c54ce127563eb0635af80ae42fc4

--- extract-files.sh
+++ extract-files.sh
@@ -60,4 +60,8 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
+BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+sed -i "s|/system/etc/camera|/vendor/etc/camera|g" "$BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
+
 "$MY_DIR"/setup-makefiles.sh
